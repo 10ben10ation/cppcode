00e1a37f876aafe1c525c76a26c59185e8faf19e
10ben10ation <73029203+10ben10ation@users.noreply.github.com>
2020-10-19 22:56:53 +0530

    Create Inheritance.cpp

---
+++ Inheritance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class C1{
+public:
+void fun1(){
+cout<<"I am fun1";
+}
+};
+class C2: public C1{
+public:
+void fun1(){
+fun1();
+cout<<"I am fun2";
+}
+};
+int main(){
+
+C2 example;
+example.fun2();
+
+return 0;
+}
